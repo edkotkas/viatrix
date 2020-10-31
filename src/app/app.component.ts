0cd0bcfd53983df181ac0f07471003207c7e39eb
Eduard Kotkas <edkotkas@gmail.com>
2020-10-31 21:24:38 +0000

    add basic timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TimeInterval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'viatrix';
+
+  private timer;
+  private currentTime = 0;
+
+  get time(): string {
+    if (this.currentTime > 60) {
+      return `${this.currentTime / 60} minutes`;
+    }
+
+    return `${this.currentTime} seconds`;
+  }
+
+  startTimer(): void {
+    this.timer = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
+  }
+
+  stopTimer(): void {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+  resetTimer(): void {
+    this.stopTimer();
+    this.currentTime = 0;
+  }
 }
